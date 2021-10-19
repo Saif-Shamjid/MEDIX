31f219cb3b5f5aa132f1f0f90194757ec5387dd2
saif.desktop <shamjid.saif@gmail.com>
2021-10-19 15:44:16 +0600

    banner added successful

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <div className='banner'>
+
+                <div className="content">
+                    <h1>
+                    GIVING CHILDREN
+                    <br />
+                    <span>THE CARE THEY DESERVE</span>
+                    </h1>
+                    <p>The bold mission of America’s MEDITEX Companies is to bring an end to the burdens of disease, in all its forms.</p>
+                    <a href="/">APPOINTMENT</a>
+                </div>  
+
+                <img className='banner-image' src="./415-removebg-preview.png" alt="" />
+
+                </div>
+        </div>
+    );
+};
+
+export default Banner;
